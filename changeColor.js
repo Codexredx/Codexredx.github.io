12c495b7399da2b2f73f3142d4ebb593ed1363d4
Codexredx <141426434+Codexredx@users.noreply.github.com>
2024-12-12 01:55:09 +0200

    Update changeColor.js

--- changeColor.js
+++ changeColor.js
@@ -6,7 +6,13 @@ function changeColor()
     {
         if (div.classList.contains('line'))
         {
-            div.style.color = 'red';
+            div.style.backgroundcolor = 'black';
+            div.style.color = 'white';
+        }
+        else
+        {
+            div.style.backgroundcolor = 'white';
+            div.style.color = 'black';
         }
     });
 }
